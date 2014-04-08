ecc9ce47dfec1256f3a1641b37faf0924c6be8e6
Alexander Adeniyin Ewetumo <trinoxf@gmail.com>
2014-04-08 08:49:26 +0100

    first commit

---
+++ lib/spark.dart
@@ -0,0 +1,14 @@
+library spark;
+
+import 'package:sparkflow/sparkflow.dart';
+import 'package:hub/hubclient.dart';
+import 'package:spark_elements/elements.dart';
+
+class DOMElement extends Element{
+  
+    void open(){}
+    void closed(){}
+    void hide(){}
+    void show(){}
+
+}
