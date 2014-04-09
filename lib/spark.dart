95caee3acd9c59c091d661275fa020b82dd66383
Alexander Adeniyin Ewetumo <trinoxf@gmail.com>
2014-04-09 07:49:05 +0100

    first steps

--- lib/spark.dart
+++ lib/spark.dart
@@ -4,6 +4,13 @@ import 'package:sparkflow/sparkflow.dart';
 import 'package:hub/hubclient.dart';
 import 'package:spark_elements/elements.dart';
 
+class Spark{
+  static registerComponents(){
+      Component.registerComponents();
+      /* SparkRegistry.register('spark'); */
+  }
+}
+
 class DOMElement extends Element{
   
     void open(){}
---
+++ web/spark.dart
@@ -0,0 +1,11 @@
+library spark.elements.spec;
+
+import 'dart:html';
+import 'package:sparkflow/sparkflow.dart';
+import 'package:hub/hubclient.dart';
+import 'package:spark_elements/elements.dart';
+
+void main(){
+    
+
+}
