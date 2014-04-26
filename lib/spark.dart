e8042d29ce4c85671f9998d7f17e8d9ad7a26052
Alexander Adeniyin Ewetumo <trinoxf@gmail.com>
2014-04-26 08:56:07 +0100

    added strips

--- lib/spark.dart
+++ lib/spark.dart
@@ -10,7 +10,11 @@ class Spark{
       Component.registerComponents();
       Elements.registerComponents();
       SparkRegistry.register('spark','domcards',DOMCards.create);
+      SparkRegistry.register('spark','eventfilter',EventFilter.create);
+      SparkRegistry.register('spark','eventreactor',EventReactor.create);
   }
+
+  static isEvent(n) => n is html.Event;
 }
 
 class DOMCards extends Element{
@@ -66,38 +70,36 @@ class DOMCards extends Element{
       this.port('events:sub').send('drop');
       this.port('events:sub').send('invalid');
 
-      this.port('events:sub').send('mouseOver');
-      this.port('events:sub').send('mouseMove');
-      this.port('events:sub').send('mouseOut');
-      this.port('events:sub').send('mouseDown');
-      this.port('events:sub').send('mouseUp');
-      this.port('events:sub').send('mouseLeave');
-      this.port('events:sub').send('mouseEnter');
-      this.port('events:sub').send('mousewheel');
-      this.port('events:sub').send('mouseEnter');
-      this.port('events:sub').send('mouseLeave');
-      this.port('events:sub').send('dragOver');
-      this.port('events:sub').send('dragEnd');
-      this.port('events:sub').send('dragStart');
-      this.port('events:sub').send('dragEnter');
+      /*this.port('events:sub').send('mouseover');*/
+      /*this.port('events:sub').send('mousemove');*/
+      /*this.port('events:sub').send('mouseout');*/
+      /*this.port('events:sub').send('mousedown');*/
+      /*this.port('events:sub').send('mouseup');*/
+      /*this.port('events:sub').send('mouseleave');*/
+      /*this.port('events:sub').send('mousewheel');*/
+      /*this.port('events:sub').send('mouseenter');*/
+      this.port('events:sub').send('dragover');
+      this.port('events:sub').send('dragend');
+      this.port('events:sub').send('dragstart');
+      this.port('events:sub').send('dragenter');
       this.port('events:sub').send('dragleave');
-      this.port('events:sub').send('fullscreenChange');
-      this.port('events:sub').send('fullscreenError');
+      /*this.port('events:sub').send('fullscreenchange');*/
+      /*this.port('events:sub').send('fullscreenerror');*/
       this.port('events:sub').send('keydown');
       this.port('events:sub').send('keyup');
       this.port('events:sub').send('keypress');
-      this.port('events:sub').send('selectStart');
-      this.port('events:sub').send('touchCancel');
-      this.port('events:sub').send('touchLeave');
-      this.port('events:sub').send('touchEnter');
-      this.port('events:sub').send('touchStart');
-      this.port('events:sub').send('touchMove');
-      this.port('events:sub').send('transitionEvent');
+      this.port('events:sub').send('selectstart');
+      this.port('events:sub').send('touchcancel');
+      this.port('events:sub').send('touchleave');
+      this.port('events:sub').send('touchenter');
+      this.port('events:sub').send('touchstart');
+      this.port('events:sub').send('touchmove');
+      this.port('events:sub').send('transitionevent');
 
-      this.port('events:sub').send('beforeCut');
-      this.port('events:sub').send('beforeCopy');
-      this.port('events:sub').send('beforePaste');
-      this.port('events:sub').send('contextMenu');
+      this.port('events:sub').send('beforecut');
+      this.port('events:sub').send('beforecopy');
+      this.port('events:sub').send('beforepaste');
+      this.port('events:sub').send('contextmenu');
       
     });
 
@@ -125,11 +127,128 @@ class DOMCards extends Element{
 
 
 
-class EventReactor{
+class EventFilter extends Component{
+  var allowed = "all";
+  bool reverse = false;
+
+  static create() => new EventFilter();
+
+  EventFilter(){
+
+    this.createSpace('ev');
+    this.createSpace('state');
+    this.createSpace('type');
+
+    this.makeInport('type:allowed',
+        meta:{'description':'takes a string or list of allowed event names' });
+    this.makeInport('ev:events',
+        meta:{'description':"the port where all events comes in"});
+    this.makeInport('state:invert',
+        meta:{'description':"takes a bool operation that inverts the operation"});
+
+    this.makeOutport('state:pass');
+    this.makeOutport('state:fail');
+    
+    this.port('state:invert').forceCondition(Valids.isBool);
+    this.port('ev:events').forceCondition(Spark.isEvent);
+
+    this.port('state:invert').tap((n){
+      this.reverse = n.data;
+    });
+
+    this.port('type:allowed').forceCondition((n){
+      return (Valids.isString(n) || Valids.isList(n) ? true : false);
+    });
+
+    this.port('type:allowed').tap((n){
+      this.allowed = n.data;
+    });
+  
+    this.port('ev:events').tap(this.filter);
+
+  }
+
+  void _handle(fn,fe,n){
+      var type = n.has('eventType') ? n.get('eventType') : n.data.type;
+
+      if(Valids.isString(this.allowed)){
+      
+        Funcs.when(Valids.match(this.allowed,"all"),(){
+           return fn(n);
+        });
+
+        Funcs.when(Valids.match(this.allowed,"none"),(){
+           return fe(n);
+        });
+
+        Funcs.when(Valids.match(type,this.allowed),(){
+           return fn(n);
+        },(){
+          return fe(n);
+        });
+  
+        return null;
+      }
+
+
+      if(Valids.isList(this.allowed))
+        return Funcs.when(this.allowed.contains(type),(){
+          return fn(n);
+        },(){
+          return fe(n);
+        });
+
+      return fe;
+  }
+
+  void filter(m){
+
+    Funcs.when(this.reverse,(){
+
+       return this._handle((n){
+          return this.sendFailure(n);
+       },(n){
+          return this.port('state:pass').send(n);
+       },m);
+
+    },(){
+        
+       return this._handle((n){
+          return this.port('state:pass').send(n);
+       },(n){
+          return this.sendFailure(n);
+       },m);
+
+    });
+
+
+  }
+
+  void sendFailure(n){
+    var fail = this.port('state:fail');
+    if(fail.hasSubscribers) return fail.send(n);
+    return null;
+  }
+
+}
+
+class EventReactor extends Component{
 
   static create() => new EventReactor();
 
   EventReactor(){
+    
+      this.enableSubnet();
+      this.createSpace('evin');
+
+      this.makeInport('evin:events',meta:{'desc':"stream of events"});
+      this.makeInport('evin:type',meta:{"desc":"type/types of events to allow"});
+      this.makeInport('evin:condfn',meta:{"desc":"a function to apply for "});
+      
+      this.port('static:option').bindPort(this.port('evin:type'));
+
+      
 
   }
+
 }
--- web/spark.dart
+++ web/spark.dart
@@ -11,21 +11,34 @@ void main(){
    Spark.registerComponents();
   
    var list = SparkRegistry.generate('spark/domcards');
+   var filter = SparkRegistry.generate('spark/eventfilter');
    var query = SparkRegistry.generate('elements/querySelector');
   
-   list.boot();
-   query.boot();
-
-   query.port('in:elem').tap(Funcs.compose(print,(e){ return "elem:$e"; }));
-   query.port('in:query').tap(Funcs.compose(print,(e){ return "query:$e"; }));
-   query.port('out:val').tap(Funcs.compose(print,(e){ return "out:val:$e"; }));
+   list.port('out:elem').tap(Funcs.compose(print,(e){ return "listElem:$e"; }));
+   list.port('events:events').tap(Funcs.compose(print,(e){ 
+     return "listEvents:$e ${e.get('eventType')} ${ e.data.type}"; 
+   }));
+   filter.port('state:pass').tap(Funcs.compose(print,(e){ 
+     return "passedFilter:$e ${e.get('eventType')} ${ e.data.type}"; 
+   }));
+   filter.port('state:fail').tap(Funcs.compose(print,(e){ 
+     return "failedFilter:$e ${e.get('eventType')} ${ e.data.type}"; 
+   }));
 
    query.port('out:val').bindPort(list.port('in:elem'));
+   list.port('events:events').bindPort(filter.port('ev:events'));
 
-   list.port('out:elem').tap(Funcs.compose(print,(e){ return "listElem:$e"; }));
-   list.port('events:events').tap(Funcs.compose(print,(e){ return "listEvents:$e"; }));
   
    query.port('in:query').send('#component_lists');
    query.port('in:elem').send(document.body);
 
+   /*new Time(const Duration(seconds:3),(t){*/
+     filter.port('type:allowed').send("none");
+   /*});*/
+
+   /*new Time(const Duration(seconds:5),(t){*/
+     filter.port('type:allowed').send("drag");
+   /*});*/
+
+     filter.port('state:invert').send(true);
 }
